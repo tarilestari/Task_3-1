f0378ba7e3dbcffbce3d855e0ba409218945e96e
Tari Lestari <tarilessstari@gmail.com>
2016-02-03 15:56:54 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,34 @@
 
+//Nama : Tari Lestari
+//Kelas: IF-38-01
+//NIM  : 1301144281
+
+public class AppStore{
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+	
+	
+	public void createNewApp (String appName, int appSize){
+		Application app = new Application (appName,appSize);
+		appList[totalApp]=app;
+		totalApp++;
+	}
+	
+	public Application getApp (int id){
+		return appList[id];
+	}
+	
+	public int getTotalApp(Application app){
+		return totalApp;
+	}
+	
+	public Application[] getAppList(){
+		return appList;
+	} 
+	
+	public String toString(){
+		return ("There are"+totalApp+" applications ready to install");
+	}
+}
+
+	
