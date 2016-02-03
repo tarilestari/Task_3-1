cf3fd8dd7befa399e0ee6fdb09ca51987e16d5b2
Tari Lestari <tarilessstari@gmail.com>
2016-02-03 15:15:41 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,45 @@
+//Nama : Tari Lestari
+//Kelas: IF-38-01
+//NIM  : 1301144281
 
+public class Member {
+	private String name;
+	private String specialization;
+	private int projectWorked=0;
+ 
+	public Member(String name) {
+        this.name = name;
+    }
+ 
+	public Member(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+	public void setName(String name) {
+		this.name = name;
+    }
+	
+	public String getSpecialization() {
+		return specialization;
+   }
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	public int getProjectWorked() {
+         projectWorked++;
+         return projectWorked;
+    }
+	
+	public void setProjectWorked(int projectWorked) {
+         this.projectWorked = projectWorked;
+    }
+	 
+	public String toString(){
+		return ("Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" projec(s)");
+	}
+}
