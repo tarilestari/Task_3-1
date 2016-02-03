44d320d6c7c3db4a95f66d47058af7c9822bb71c
Tari Lestari <tarilessstari@gmail.com>
2016-02-03 15:57:03 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,34 @@
 
+//Nama : Tari Lestari
+//Kelas: IF-38-01
+//NIM  : 1301144281
+
+public class Application {
+	private String appName;
+	private int appSize;
+
+	
+	public Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName (){
+		return appName;
+	}
+	
+	public int getAppSize (){
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name: "+appName+",with size:"+appSize+"MB");
+	}
+}
+
+
+
+
+	
+	
+	
