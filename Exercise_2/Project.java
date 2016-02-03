3d707dcd41879028fca9cbbe51233bd59b35b8a9
Tari Lestari <tarilessstari@gmail.com>
2016-02-03 15:57:22 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,38 @@
+//Nama : Tari Lestari
+//Kelas: IF-38-01
+//NIM  : 1301144281
 
+public class Project {
+	private String projectName;
+	private Member[] teamMember = new Member[5];
+	private int nTeam = 0;
+	private boolean releaseStatus = false;
+	
+	public Project(String projectName) {
+		this.projectName = projectName;
+	}
+ 	
+ 	public void addMember(Member m) {
+ 		teamMember[nTeam] = m;
+ 		nTeam++;
+ 	}
+ 	
+ 	public boolean isReleased() {
+ 		return releaseStatus;
+ 	}
+ 	
+ 	public void releaseApp() {
+ 		releaseStatus = true;
+ 	}
+ 	
+ 	public String toString() {
+ 		String kata = "";
+ 		if (releaseStatus == true) {
+ 			kata = "Project " + projectName + " status is complete with team member of " + nTeam;
+ 		}
+ 		else {
+ 			kata = "Project " + projectName + " status is in progress with team member of " + nTeam;
+ 		}
+ 		return kata; 
+ 	}
+ }
